10a97a35885f75f206d188d55042c2b0c83ca794
Steven Good <stevieg.bitbucket@gmail.com>
2016-03-03 23:31:15 +0000

    wip

--- src/main/java/com/comeragh/examples/java_pipeline/AMQPQueueSink.java
+++ src/main/java/com/comeragh/examples/java_pipeline/AMQPQueueSink.java
@@ -3,20 +3,45 @@ package com.comeragh.examples.java_pipeline;
 import org.apache.commons.io.input.Tailer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class AMQPQueueSink implements Runnable
 {
     private static final Logger logger = LogManager.getLogger(AMQPQueueSink.class);
 
     private final BlockingQueue<String> queue;
+    private final String host;
+    private final int port;
+    private final String vhost;
+    private final String qname;
 
-    AMQPQueueSink(, BlockingQueue<String> in)
+    AMQPQueueSink(String propfilename, BlockingQueue<String> in)
     {
+    	host = h;
+    	port = p;
+    	vhost = vh;
+    	qname = q;
     	queue = in;
+    	
+    	
 
     }
 
+    public void run()
+    {
+    	while(!Thread.currentThread().isInterrupted())
+    	{
+    		String s = queue.poll(100, TimeUnit.MILLISECONDS);
+    		if (s != null)
+    			ampq.put(s);
+    	}
+    }
+    
     public static void main(String[] args)
 	{
 		logger.info("Running main method");
