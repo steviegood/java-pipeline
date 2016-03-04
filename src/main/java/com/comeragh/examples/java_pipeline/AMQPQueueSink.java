7bb5f65571d2af7bdda0ad4ae8090df441bef0aa
Steven Good <stevieg.bitbucket@gmail.com>
2016-03-04 23:41:44 +0000

    Got messages into QPID

--- src/main/java/com/comeragh/examples/java_pipeline/AMQPQueueSink.java
+++ src/main/java/com/comeragh/examples/java_pipeline/AMQPQueueSink.java
@@ -1,13 +1,15 @@
 package com.comeragh.examples.java_pipeline;
 
-import org.apache.commons.io.input.Tailer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import javax.jms.Connection;
 import javax.jms.Session;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.MessageProducer;
+import javax.jms.TextMessage;
 import org.apache.qpid.client.AMQConnection;
+import org.apache.qpid.client.AMQAnyDestination;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import java.util.Properties;
@@ -18,27 +20,33 @@ public class AMQPQueueSink implements Runnable
 {
     private static final Logger logger = LogManager.getLogger(AMQPQueueSink.class);
 
-    private final BlockingQueue<String> queue;
+    private final BlockingQueue<String> inboundQueue;
+    private final Connection connection;
+    private final Session session;
+    private final Destination outboundQueue;
+    private final MessageProducer producer;
+    
     //private final String host;
     //private final int port;
     //private final String vhost;
     //private final String qname;
 
-    AMQPQueueSink(String propfilename, BlockingQueue<String> in)
+    AMQPQueueSink(BlockingQueue<String> in) throws Exception
     {
-    	
-    	Connection conn = new AMQConnection("amqp://gues:guest@test/?brokerlist='tcp://localhost:5672'");
-    	Session sess = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-    	Desintation queue = new AMQAnyDestination("ADDR:message_queue; {create,always}");
-    	MessageProducer producer = session.createProducer(queue);
+    	//Properties properties = new Properties();
+
+    	inboundQueue = in;
+    	connection = new AMQConnection("amqp://guest:guest@AMQPQueueSink/bdb?brokerlist='tcp://localhost:5672'");
+    	session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    	//outboundQueue = new AMQAnyDestination("ADDR:message_queue; {create,always}");
+    	outboundQueue = new AMQAnyDestination("test_queue");
+    	producer = session.createProducer(outboundQueue);
     	//host = h;
     	//port = p;
     	//vhost = vh;
     	//qname = q;
-    	queue = in;
+    	//queue = in;
     	// TO DO
-    	
-    	
     }
 
     public void run()
@@ -48,15 +56,37 @@ public class AMQPQueueSink implements Runnable
     		String s = null;
 			try 
 			{
-				s = queue.poll(100, TimeUnit.MILLISECONDS);
+				s = inboundQueue.poll(100, TimeUnit.MILLISECONDS);
 			}
 			catch (InterruptedException e)
 			{
 				return;
 			}
     		if (s != null)
-    			//ampq.put(s);
-    			System.out.println("Blah");;
+    		{
+    			TextMessage message;
+				try 
+				{
+					message = session.createTextMessage(s);
+				}
+				catch (JMSException e)
+				{
+					e.printStackTrace();
+					return;
+				}
+    			try
+    			{
+    				logger.info("About to send message");
+					producer.send(message);
+					logger.info("Just sent message");
+				}
+    			catch (JMSException e) 
+    			{
+					e.printStackTrace();
+				    return;
+				}
+    		}
+
     	}
     }
     
---
+++ src/main/java/com/comeragh/examples/java_pipeline/XMLToAMQPTest.java
@@ -0,0 +1,54 @@
+package com.comeragh.examples.java_pipeline;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class XMLToAMQPTest
+{
+	private static final Logger logger = LogManager.getLogger(XMLToAMQPTest.class);
+	private final LinkedBlockingQueue<String> fts2xap;
+	private final LinkedBlockingQueue<String> xap2ams;
+    private final Runnable fts;
+    private final Runnable xap;
+    private final Runnable ams;
+    private final Executor executor;
+
+    XMLToAMQPTest(String fname) throws Exception
+    {
+    	fts2xap = new LinkedBlockingQueue<String>();
+    	xap2ams = new LinkedBlockingQueue<String>();
+        fts = new FileTailerSource(fname, true, fts2xap);
+    	xap = new XMLAssemblerPipe(fts2xap,xap2ams);
+    	ams = new AMQPQueueSink(xap2ams);
+    	executor = Executors.newCachedThreadPool();
+    }
+    
+    void start()
+    {
+        executor.execute(fts);
+        executor.execute(xap);
+        executor.execute(ams);
+    }
+    
+    public static void main(String[] args)
+	{
+		logger.info("Running main method");
+		XMLToAMQPTest stt = null;
+        try
+        {
+        	stt = new XMLToAMQPTest("/Users/steve/test.log");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        if (stt != null)
+        {
+        	logger.info("Here we go!!");
+            stt.start();
+        }
+    }
+}
